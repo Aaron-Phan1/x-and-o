50ef3173ad4644ba1a849d46e8e93fd1fe50f6a1
Aaron <aaron.phan1@hotmail.com>
2024-10-01 17:24:29 +0800

    Add hard and easy mode colour constants
    This ensures consistent styling across game files

--- x-and-o/main.lua
+++ x-and-o/main.lua
@@ -2,6 +2,7 @@ local composer = require( "composer" )
  
 -- Code to initialize your app
 d = display
+w1_25 = d.contentWidth * .0125
 w2_5 = d.contentWidth * .025
 w5 = d.contentWidth * .05
 w10 = d.contentWidth * .1
@@ -14,6 +15,7 @@ w70 = d.contentWidth * .7
 w80 = d.contentWidth * .8
 w90 = d.contentWidth * .9
 
+h1_25 = d.contentHeight * .0125
 h2_5 = d.contentHeight * .025
 h5 = d.contentHeight * .05
 h10 = d.contentHeight * .1
@@ -25,5 +27,8 @@ h60 = d.contentHeight * .6
 h70 = d.contentHeight * .7
 h80 = d.contentHeight * .8
 h90 = d.contentHeight * .9
--- Assumes that "scene1.lua" exists and is configured as a Composer scene
+
+
+hardColour = {1, 0.45, 0.55}
+easyColour = {0.45, 0.9, 1}
 composer.gotoScene( "profile_selection" )
